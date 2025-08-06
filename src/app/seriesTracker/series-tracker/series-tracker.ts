409ea68f88baed9ac109a1220a87fb03e28c85fc
Ksanen <ksawiercz1222@gmail.com>
2025-08-06 21:38:35 +0200

    adding option to close menu and open it and adding some styles

--- src/app/seriesTracker/series-tracker/series-tracker.ts
+++ src/app/seriesTracker/series-tracker/series-tracker.ts
@@ -5,6 +5,7 @@ import { SeriesAdd } from '../components/series-add/series-add';
 import { CommonModule } from '@angular/common';
 import { SeriesViewSettings } from '../components/series-view-settings/series-view-settings';
 import { SeriesFilter } from '../components/series-filter/series-filter';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'series-tracker',
   imports: [
@@ -22,7 +23,12 @@ import { SeriesFilter } from '../components/series-filter/series-filter';
 export class SeriesTracker {
   name: WritableSignal<string> = signal('');
   addSeriesIsOpen: boolean = false;
+  asideIsOpen: boolean = false;
+  constructor(private cd: ChangeDetectorRef) {}
   toggleAddSeriesIsOpen() {
     this.addSeriesIsOpen = !this.addSeriesIsOpen;
+  }
+  toggleAside() {
+    this.asideIsOpen = !this.asideIsOpen;
   }
 }
