3fff49de8814596381c53980594e8625cc120fef
Ksanen <ksawiercz1222@gmail.com>
2025-08-13 21:26:32 +0200

    adding env variable for port

--- server/server.mjs
+++ server/server.mjs
@@ -16,7 +16,7 @@ app.use(
 );
 app.use(routes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`server is listening on port ${PORT}`);
